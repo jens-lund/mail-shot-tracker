6010e96154c74e37c929ff52c37ebf4ee04f6c88
Mail Project <mail-project@example.com>
2026-05-19 12:37:03 +0200

    Configure GitHub sync target

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.MAIL_TRACKER_CONFIG = {
   projectName: "Mail",
   editPasswordHash: "00d8d3f11739d2f3537099982b4674c29fc59a8fda350fca1379613adbb09119",
   github: {
-    owner: "",
-    repo: "",
+    owner: "jens-lund",
+    repo: "mail-shot-tracker",
     branch: "main",
     dataPath: "data/shots.json"
   }
